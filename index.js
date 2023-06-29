47defbbd6304b6cfcd7de17565f59d32689e73eb
Bohun <maks.boggun@gmail.com>
2023-06-29 09:51:18 +0300

    created server and simple api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 //////////////// FILES
 
@@ -30,3 +31,31 @@ const http = require("http");
 //     });
 //   });
 // });
+
+//////////////// SERVER
+
+const apiData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const apiDataObj = JSON.parse(apiData);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  const pathName = req.url;
+  if (pathName === "/overview" || pathName === "/") {
+    res.end("This is the OVERVIEW ");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT ");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(apiData);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
