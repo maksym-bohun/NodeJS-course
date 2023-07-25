b4577cf29f878801075298c75f529a33dbea7f99
Bohun <maks.boggun@gmail.com>
2023-07-25 13:49:41 +0300

    catching uncaught exceptions

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught exception! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -24,8 +30,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('Unhandled rejection! 💥 Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
