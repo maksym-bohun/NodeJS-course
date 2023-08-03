94ca1c5a516cf9a061fe771439398c7b752dcfb1
Bohun <maks.boggun@gmail.com>
2023-08-03 17:54:12 +0300

    implemented simple nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const toursController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -26,6 +27,17 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     toursController.deleteTour
+  );
+
+// POST /tours/242dusd/reviews
+// GET /tours/242dusd/reviews
+// GET /tours/242dusd/reviews/73987ajash
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
   );
 
 module.exports = router;
